db46561772f5f5d22c9f0aface57b28292a83c69
FajarKyuraa <fajarsasongko086@gmail.com>
2019-07-01 12:49:57 +0700

    abis lembur

--- History/History.js
+++ History/History.js
@@ -1,21 +1,160 @@
-function load1() {
+function load1963() {
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 	  	document.getElementById("demo").innerHTML = this.responseText;
 		}
 	};
-	xhttp.open("GET", "ajax_info.txt", true);
+	xhttp.open("GET", "1963.txt", true);
 	xhttp.send();
 }
-
-function load2() {
+function load1965() {
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 	  	document.getElementById("demo").innerHTML = this.responseText;
 		}
 	};
-	xhttp.open("GET", "hasil2.txt", true);
+	xhttp.open("GET", "1965.txt", true);
+	xhttp.send();
+}
+function load1967() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+	  	document.getElementById("demo").innerHTML = this.responseText;
+		}
+	};
+	xhttp.open("GET", "1967.txt", true);
+	xhttp.send();
+}
+function load1969() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+	  	document.getElementById("demo").innerHTML = this.responseText;
+		}
+	};
+	xhttp.open("GET", "1969.txt", true);
+	xhttp.send();
+}
+function load1972() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+	  	document.getElementById("demo").innerHTML = this.responseText;
+		}
+	};
+	xhttp.open("GET", "1972.txt", true);
+	xhttp.send();
+}
+function load1974() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+	  	document.getElementById("demo").innerHTML = this.responseText;
+		}
+	};
+	xhttp.open("GET", "1974.txt", true);
+	xhttp.send();
+}
+function load1978() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+	  	document.getElementById("demo").innerHTML = this.responseText;
+		}
+	};
+	xhttp.open("GET", "1978.txt", true);
+	xhttp.send();
+}
+function load1981() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+	  	document.getElementById("demo").innerHTML = this.responseText;
+		}
+	};
+	xhttp.open("GET", "1981.txt", true);
+	xhttp.send();
+}
+function load1984() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+	  	document.getElementById("demo").innerHTML = this.responseText;
+		}
+	};
+	xhttp.open("GET", "1894.txt", true);
+	xhttp.send();
+}
+function load1987() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+	  	document.getElementById("demo").innerHTML = this.responseText;
+		}
+	};
+	xhttp.open("GET", "1987.txt", true);
+	xhttp.send();
+}
+function load1991() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+	  	document.getElementById("demo").innerHTML = this.responseText;
+		}
+	};
+	xhttp.open("GET", "1991.txt", true);
+	xhttp.send();
+}
+function load1994() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+	  	document.getElementById("demo").innerHTML = this.responseText;
+		}
+	};
+	xhttp.open("GET", "1994.txt", true);
+	xhttp.send();
+}
+function load1998() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+	  	document.getElementById("demo").innerHTML = this.responseText;
+		}
+	};
+	xhttp.open("GET", "1998.txt", true);
+	xhttp.send();
+}
+function load2005() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+	  	document.getElementById("demo").innerHTML = this.responseText;
+		}
+	};
+	xhttp.open("GET", "2005.txt", true);
+	xhttp.send();
+}
+function load2008() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+	  	document.getElementById("demo").innerHTML = this.responseText;
+		}
+	};
+	xhttp.open("GET", "2008.txt", true);
+	xhttp.send();
+}
+function load2014() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+	  	document.getElementById("demo").innerHTML = this.responseText;
+		}
+	};
+	xhttp.open("GET", "2014.txt", true);
 	xhttp.send();
 }
